f36372220ce741b019c818052764d95092f6129a
DukeDesign <aaronduke22@gmail.com>
2017-05-01 15:45:10 +0100

    Webkit 0%
    all webkits

--- js/scripts.js
+++ js/scripts.js
@@ -30,7 +30,7 @@ $('html').scroll(function() {
 window.onscroll = function() {myFunction()};
 
 function myFunction() {
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
         document.getElementById("header").className = "scroll";
         document.getElementById("logo").className = "scrollLogo";
     } else {
@@ -65,13 +65,13 @@ function check_if_in_view() {
   var window_height = $window.height();
   var window_top_position = $window.scrollTop();
   var window_bottom_position = (window_top_position + window_height);
- 
+
   $.each($animation_elements, function() {
     var $element = $(this);
     var element_height = $element.outerHeight();
     var element_top_position = $element.offset().top;
     var element_bottom_position = (element_top_position + element_height);
- 
+
     //check to see if this current container is within viewport
     if ((element_bottom_position >= window_top_position) &&
         (element_top_position <= window_bottom_position)) {
